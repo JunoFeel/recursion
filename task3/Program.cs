086c7cc5ae06e8a2d1be773693d656727e126ee2
Yuliya Anpilogova <junofeel@mail.ru>
2024-07-26 18:33:09 +0300

    добавлено решение задачи 3

---
+++ task3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 3: Задайте произвольный массив. 
+// Выведете его элементы, начиная с конца. 
+// Использовать рекурсию, не использовать циклы.
+
+
+
+void MassivInReverseOrder (int[] numbers, int index)
+{
+    {
+        if (index < 0)
+        {
+            return;
+        }
+
+        Console.WriteLine(numbers[index]);
+        MassivInReverseOrder(numbers, index - 1);
+    }
+}
+
+int[] numbers = { 1, 2, 3, 4, 5 };
+MassivInReverseOrder(numbers, numbers.Length - 1);
