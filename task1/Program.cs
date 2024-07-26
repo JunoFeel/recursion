b0cb4907d2c4d0d0ebafc727ad69303928cc81a1
Yuliya Anpilogova <junofeel@mail.ru>
2024-07-26 18:27:30 +0300

    добавлено решение задачи 1

---
+++ task1/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 1: Задайте значения M и N. 
+// Напишите программу, которая выведет 
+// все натуральные числа в промежутке от M до N. 
+// Использовать рекурсию, не использовать циклы.
+
+
+
+bool Proverka (int m, int n)
+{
+    if (n < m)
+    {
+        Console.WriteLine("Число M должно быть больше числа  N");
+        return true;
+    }
+    return false;
+}
+
+void NaturalNumbersBetween(int m, int n)
+{
+    if (m <= n)
+    {
+        Console.WriteLine($" {m}");
+        m = m + 1;
+        NaturalNumbersBetween(m,n);
+    }
+}
+
+Console.WriteLine("Введите значение M");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение N");
+int n = Convert.ToInt32(Console.ReadLine());
+Proverka(m,n);
+NaturalNumbersBetween(m,n);
